b9f38ae57e567c91d0cf6a1d630827c864ea4a79
ainhoahdez <a.hernandezs.2016@alumnos.urjc.es>
2020-12-14 10:24:03 +0100

    "Hecho el año bisiesto"

--- Practica_CN/src/Bisiestos/Bisiestos.java
+++ Practica_CN/src/Bisiestos/Bisiestos.java
@@ -1,5 +1,13 @@
 package Bisiestos;
 
 public class Bisiestos {
-
+	
+	// @param a un número entero positivo
+	// @return true si a es un año bisiesto
+	// false en caso contrario.
+	// @throws InvalidParameter si a no es un parámetro válido.
+	public boolean esBisiesto(int a) throws InvalidParameter {
+		return ((a % 4 == 0) && ((a % 100 != 0) || (a % 400 == 0)));
+	}
 }
+
