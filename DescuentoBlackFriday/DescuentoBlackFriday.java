b04223c245e458e3ecae3858f93fa39d0c46a9a0
RocioNaVi <r.navarro.2016@alumnos.urjc.es>
2020-12-14 17:18:17 +0100

    Cambio a IllegalArgument

--- DescuentoBlackFriday/DescuentoBlackFriday.java
+++ DescuentoBlackFriday/DescuentoBlackFriday.java
@@ -1,22 +1,22 @@
 package DescuentoBlackFriday;
 
-import java.security.InvalidParameterException;
+import java.lang.IllegalArgumentException;
 
 public class DescuentoBlackFriday {
 	// @param  precioOriginal es el precio de un producto marcado en la etiqueta
 	//         porcentajeDescuento es el descuento a aplicar expresado como un porcentaje
 	// @return el precio final teniendo en cuenta que si es black friday (29 de noviembre) se aplica
 	//         un descuento de porcentajeDescuento
-	// @throws InvalidParameter si precioOriginal es negativo
+	// @throws IllegalArgumentException si precioOriginal es negativo
 	
-	public static double precioFinal(double precioOriginal, double porcentajeDescuento) throws InvalidParameterException{
+	public static double precioFinal(double precioOriginal, double porcentajeDescuento) throws IllegalArgumentException{
 		
 		double precioFinal;
 
-		if (porcentajeDescuento > 100 || porcentajeDescuento < 0 || precioOriginal < 0)
+		if (porcentajeDescuento < 0 || precioOriginal < 0)
 	       {
 			  System.out.println("Se ha introducido un valor incorrecto");
-	          throw new InvalidParameterException ("DescuentoBlackFriday.precioFinal");
+	          throw new IllegalArgumentException ("DescuentoBlackFriday.precioFinal");
 	       }
 		precioFinal = precioOriginal*porcentajeDescuento/100;
 		precioFinal = precioOriginal-precioFinal;
