2aadf6f97485be842ca34109830a2feb283e1a04
RocioNaVi <r.navarro.2016@alumnos.urjc.es>
2020-12-16 11:41:41 +0100

    Modificación fecha en código

--- DescuentoBlackFriday/DescuentoBlackFriday.java
+++ DescuentoBlackFriday/DescuentoBlackFriday.java
@@ -1,6 +1,8 @@
 package DescuentoBlackFriday;
 
 import java.lang.IllegalArgumentException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class DescuentoBlackFriday {
 	// @param  precioOriginal es el precio de un producto marcado en la etiqueta
@@ -12,15 +14,25 @@ public class DescuentoBlackFriday {
 	public double precioFinal(double precioOriginal, double porcentajeDescuento) throws IllegalArgumentException{
 		
 		double precioFinal;
-
-		if (porcentajeDescuento < 0 || precioOriginal < 0)
-	       {
-			  System.out.println("Se ha introducido un valor incorrecto");
-	          throw new IllegalArgumentException ("DescuentoBlackFriday.precioFinal");
-	       }
-		precioFinal = precioOriginal*porcentajeDescuento/100;
-		precioFinal = precioOriginal-precioFinal;
 		
-		return precioFinal;
+		
+		Calendar fecha = new GregorianCalendar();
+	    int mes = fecha.get(Calendar.MONTH);
+		int dia = fecha.get(Calendar.DAY_OF_MONTH);
+
+		if (dia == 29 && mes == 11) 
+		{
+			if (porcentajeDescuento < 0 || precioOriginal < 0)
+		       {
+				  System.out.println("Se ha introducido un valor incorrecto");
+		          throw new IllegalArgumentException ("DescuentoBlackFriday.precioFinal");
+		       }
+			precioFinal = precioOriginal*porcentajeDescuento/100;
+			precioFinal = precioOriginal-precioFinal;
+			
+			return precioFinal;
+		}
+		
+		return precioOriginal;	
 	}
 }
