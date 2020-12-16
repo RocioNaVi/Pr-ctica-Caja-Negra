0b6a4487c1da576abfb232764455a5ff852f4722
ainhoahdez <a.hernandezs.2016@alumnos.urjc.es>
2020-12-16 01:40:07 +0100

    Cambiado numeros romanos

--- NumRomanos/NumRomanos.java
+++ NumRomanos/NumRomanos.java
@@ -1,32 +1,50 @@
 package NumRomanos;
-import org.junit.*;
+
+import java.security.InvalidParameterException;
+import java.util.Hashtable;
 
 public class NumRomanos {
+	
+	
+	public static Hashtable<String, Integer> llenarHashtable()
+	{
+		Hashtable<String, Integer> numerosRomanos = new Hashtable<String, Integer>();
+		numerosRomanos.put(" ", 0);
+		numerosRomanos.put("I", 1);
+		numerosRomanos.put("V", 5);
+		numerosRomanos.put("X", 10);
+		numerosRomanos.put("L", 50);
+		numerosRomanos.put("C", 100);
+		numerosRomanos.put("D", 500);
+		numerosRomanos.put("M", 1000);
+		
+		return numerosRomanos;	//Hashtable lleno
+	}
+	
 	
 	// @param s es un número romano
 	// @return el número s en base 10
 	// @throws InvalidParameter si s no es un número romano
-	public int convierte(String s) throws InvalidParameter {
-		char Rom[]={'','I','V','L','C','D','M'};
-		int valor[]= {0,1,5,10,50,100,500,1000};
-		int numero = 0;
+	public int convierte(String s) throws InvalidParameterException {
+		int numero = 0;	//Variable de retorno
+		int valor = 0; // Valor de la letra
 		int ant = 0;
-		boolean NotRom = True;
-		for(i=0; i<s.length(); i++) {
-			NotRom = False;
-			letra = s[i];
-			for(j=0; j<Rom.length(); j++) {
-				if(letra == Rom[j]) {
-					NotRom = False;
-					numero = numero + valor[j];
-					if(ant<valor[j]){
-						numero = numero - ant*2;
-					}
-					ant = valor[j];
+		char letra;
+		boolean esta;
+		for(int i=0; i<s.length(); i++) {
+			letra = s.charAt(i);
+			Hashtable<String, Integer> numerosRomanos = llenarHashtable();
+			esta = numerosRomanos.containsKey(Character.toString(letra).toUpperCase());
+			if(esta) {
+				valor = numerosRomanos.get(Character.toString(letra).toUpperCase());
+				numero = numero + valor;
+				if(ant<valor){
+					numero = numero - ant*2;
+				}else {
+					ant = valor;
 				}
-			}
-			if(j == Rom.length() && NotRom) {
-				throw new InvalidParameter("No es un número romano");
+			}else{
+				throw new InvalidParameterException("No es un número romano");
 			}
 		}
 		return numero;
---
+++ Practica_CN/src/NumRomanos/NumRomanosTestTest.java
@@ -0,0 +1,7 @@
+package NumRomanos;
+
+import junit.framework.TestCase;
+
+public class NumRomanosTestTest extends TestCase {
+
+}
