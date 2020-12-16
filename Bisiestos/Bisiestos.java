bce6b4ca1a1739b81f5f7fe37f1993d7bdeaea6f
ainhoahdez <a.hernandezs.2016@alumnos.urjc.es>
2020-12-16 10:59:03 +0100

    modificado bisiestos

--- Bisiestos/Bisiestos.java
+++ Bisiestos/Bisiestos.java
@@ -8,6 +8,9 @@ public class Bisiestos {
 	// @throws IllegalArgumentException si a no es un parámetro válido.
 	public boolean esBisiesto(int a) throws IllegalArgumentException 
 	{
+		if (a < 0) {
+			throw new IllegalArgumentException("No es un número romano");
+		}
 		return ((a % 4 == 0) && ((a % 100 != 0) || (a % 400 == 0)));
 	}
 }
