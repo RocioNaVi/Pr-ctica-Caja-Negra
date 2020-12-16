20d2bc9afc53e23e7a1177dde63adee7516cd380
ainhoahdez <a.hernandezs.2016@alumnos.urjc.es>
2020-12-16 15:58:01 +0100

    test Black Friday

--- DescuentoBlackFriday/DescuentoBlackFridayTest.java
+++ DescuentoBlackFriday/DescuentoBlackFridayTest.java
@@ -1,47 +1,14 @@
 package DescuentoBlackFriday;
 
 import static org.junit.Assert.assertTrue;
-
-import Embotelladora.Embotelladora;
-import Embotelladora.InvalidParameterException;
-import Embotelladora.Test;
-
-public class DescuentoBlackFridayTest {
-	private double precioOriginal, porcentajeDescuento;   // Entrada 
-	/* 
-	 * CARACTERIZACIONES:
-	 * C1: 	La entrada sea diferente de un int válido
-	 * 		B1: Precio original negativo
-	 * 		B2: Descuento negativo
-	 * 		B3: Precio original  = cero
-	 * 		B4: Descuento = cero
-	 * C3: La entrada contenga espacios en blanco
-	 * 		B1: WhiteSpace al comienzo
-	 * 		B2: WhiteSpace entre letras
-	 * 		B3: WhiteSpace al final
-	 * C4: La entrada en mayusculas o minúsculas
-	 * 		B1: Todo en minúsculas
-	 * 		B2: Se alternan mayúsculas y minúsculas
-	 */
-	
-	  /* C1.B1 */
-	  @Test (expected = IllegalArgumentException.class)
-	  public void testForNegativeIntIn_1()
-	  {
-		  precioOriginal = -40;
-		  porcentajeDescuento = 2;
-		  DescuentoBlackFriday.precioFinal (precioOriginal, porcentajeDescuento);
-	  }
-	  /* C1.B2 */
-package DescuentoBlackFriday;
-
-import static org.junit.Assert.assertTrue;
 import java.lang.IllegalArgumentException;
+import java.util.Calendar;
 
 import org.junit.Test;
 
 public class DescuentoBlackFridayTest {
 	private double precioOriginal, porcentajeDescuento;   // Entrada 
+	private Calendar date;
 	DescuentoBlackFriday descuento = new DescuentoBlackFriday();
 	/* 
 	 * CARACTERIZACIONES:
@@ -52,43 +19,74 @@ public class DescuentoBlackFridayTest {
 	 * 		B4: Descuento = cero
 	 * C2: Fecha
 	 * 		B1: Fecha válida
-	 * 		B2: Fecha no válida
+	 * 		B2: Fecha no válida por mes
+	 * 		B2. Fecha no válida por mes
 	 */
 	
 	  /* C1.B1 */
 	  @Test (expected = IllegalArgumentException.class)
 	  public void testForNegativeIntInPrecio()
 	  {
+		  date.set(2020, 11,29); 
 		  precioOriginal = -40;
 		  porcentajeDescuento = 2;
-		  descuento.precioFinal (precioOriginal, porcentajeDescuento);
+		  descuento.precioFinal (precioOriginal, porcentajeDescuento, date);
 	  }
 	  /* C1.B2 */
 	  @Test 
 	  public void testForZeroInPrecio()
 	  {
+		  date.set(2020, 11,29); 
 		  precioOriginal = 0;
 		  porcentajeDescuento = -20;
-		  assertTrue ("C1.B2", 0 ==  descuento.precioFinal (precioOriginal, porcentajeDescuento));
+		  assertTrue ("C1.B2", 0 ==  descuento.precioFinal (precioOriginal, porcentajeDescuento, date));
 	  }
 	  
 	  /* C1.B3 */
 	  @Test (expected = IllegalArgumentException.class)
 	  public void testForNegativeDesct()
 	  {
+		  date.set(2020, 11,29); 
 		  precioOriginal = 30;
 		  porcentajeDescuento = -20;
-		  descuento.precioFinal (precioOriginal, porcentajeDescuento);
+		  descuento.precioFinal (precioOriginal, porcentajeDescuento, date);
 	  }
 	  
 	  /* C1.B4 */
 	  @Test 
 	  public void testForZeroInDesc()
 	  {
+		  date.set(2020, 11,29); 
 		  precioOriginal = 20;
 		  porcentajeDescuento = 0;
-		  assertTrue ("C1.B4", 20 ==  descuento.precioFinal (precioOriginal, porcentajeDescuento));
+		  assertTrue ("C1.B4", 20 ==  descuento.precioFinal (precioOriginal, porcentajeDescuento, date));
 	  }
 	  
 	  /* C2.B1  */
+	  @Test 
+	  public void testForFechaValida()
+	  {
+		  date.set(2020, 11,29); 
+		  precioOriginal = 20;
+		  porcentajeDescuento = 10;
+		  assertTrue ("C2.B1", 18 ==  descuento.precioFinal (precioOriginal, porcentajeDescuento, date));
+	  }
+	  /* C2.B2  */
+	  @Test 
+	  public void testForFechaNoValidaMes()
+	  {
+		  date.set(2020, 10,29); 
+		  precioOriginal = 20;
+		  porcentajeDescuento = 10;
+		  assertTrue ("C2.B2", 20 ==  descuento.precioFinal (precioOriginal, porcentajeDescuento, date));
+	  }
+	  /* C2.B3  */
+	  @Test 
+	  public void testForFechaNoValidaDia()
+	  {
+		  date.set(2020, 11,30); 
+		  precioOriginal = 20;
+		  porcentajeDescuento = 10;
+		  assertTrue ("C2.B3", 20 ==  descuento.precioFinal (precioOriginal, porcentajeDescuento, date));
+	  }
 }
--- DescuentoBlackFriday/DescuentoBlackFridayTest2.java
+++ DescuentoBlackFriday/DescuentoBlackFridayTest2.java
@@ -1,8 +1,11 @@
 package DescuentoBlackFriday;
 
+package DescuentoBlackFriday;
+
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collection;
 
 import org.junit.Test;
@@ -16,20 +19,27 @@ import Embotelladora.NoSolution;
 public class DescuentoBlackFridayTest2 {
 	private double precioOriginal, porcentajeDescuento;   // Entrada 
 	private double total; //Salida
+	private static Calendar date_1, date_2;
+	private Calendar date;
+	
 	DescuentoBlackFriday descuento = new DescuentoBlackFriday();
-	public DescuentoBlackFridayTest2(double precioOri, double porcentaje, double tot){
+	public DescuentoBlackFridayTest2(double precioOri, double porcentaje, Calendar date, double tot){
 		this.precioOriginal = precioOri;
 		this.porcentajeDescuento = porcentaje;
+		this.date = date; 
 		this.total = tot;
 	}
 
 	@Parameters public static Collection<Object[]> contarbotellas(){
-		return Arrays.asList (new Object[][]{{100,5,95},{50,1,49.5},{40,10,36},{400,10,360},{92.50,50,41.25},{12,2,11.76}});
+		date_2.set(2020, 10,10);
+		date_1.set(2020, 11,29); 
+		return Arrays.asList (new Object[][]{{100,5,date_1,95},{50,1,date_1,49.5},{40,10,date_2,40},{400,10,date_2, 400},{92.50,50,date_1, 41.25},{12,2,date_1, 11.76}});
 	}
 	
 	@Test public void Botellas_Peq_Test() throws NoSolution
 	{
-		assertTrue ("calculaDescuento", total ==  descuento.precioFinal (precioOriginal, porcentajeDescuento));
+		assertTrue ("calculaDescuento", total ==  descuento.precioFinal (precioOriginal, porcentajeDescuento, date));
 	}
 }
+
 
